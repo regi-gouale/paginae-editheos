11e0dc45de00ae732ce9487e6dabdd10958123a6
Régi GOUALE <regi.gouale@gmail.com>
2025-09-07 19:53:56 +0200

    feat: add Prisma schema for User, Session, Account, and Verification models; remove unused SVG files

---
+++ lib/auth.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@/prisma/generated/prisma";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+const prisma = new PrismaClient();
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+});
