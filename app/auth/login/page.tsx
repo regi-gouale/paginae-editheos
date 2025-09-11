19a0705c8f9c4971abfdc770c2c1047c20c95a71
Régi GOUALE <regi.gouale@gmail.com>
2025-09-11 23:46:53 +0200

    feat: encapsuler les formulaires de connexion et d'inscription dans un composant Suspense

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,12 @@
 import { LoginForm } from "@/components/auth/login-form";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <div className="flex items-center justify-center min-h-screen p-8 gap-16 sm:p-20 bg-background">
-      <LoginForm />
+      <Suspense>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 }
