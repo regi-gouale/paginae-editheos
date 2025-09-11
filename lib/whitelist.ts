ae793656b9bfbe5807de72be2d1bbbd3c72295f0
Régi GOUALE <regi.gouale@gmail.com>
2025-09-11 17:14:13 +0200

    feat: ajout de nouveaux emails à la liste blanche pour les inscriptions

--- lib/whitelist.ts
+++ lib/whitelist.ts
@@ -2,5 +2,9 @@
 export const EMAIL_WHITELIST = [
   "admin@editheos.fr",
   "contact@editheos.fr",
+  "jimmy.mambou@gmail.com",
+  "ke.missions.icclyon@gmail.com",
+  "josebiab@yahoo.fr",
+  "regi.gouale@gmail.com",
   // Ajoutez ici d'autres emails autorisés
 ];
