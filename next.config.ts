65b06e2588e1a0af2c682435d8532a7122c22263
Régi GOUALE <regi.gouale@gmail.com>
2026-07-15 20:14:56 +0200

    chore: update next and add @next/bundle-analyzer dependency

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const withBundleAnalyzer = require("next-bundle-analyzer")({
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
